85a4ccb0693429d64ece989f951c0fd9275ff1e0
Yordan Iliev <jordan.iliev501@gmail.com>
2023-08-18 15:08:43 +0300

    Eval c++ submissions

--- lib/judge_worker.ex
+++ lib/judge_worker.ex
@@ -11,19 +11,46 @@ defmodule JudgeWorker do
     Logger.debug("Received event: #{inspect(event)}")
     {:ok, payload} = Jason.decode(json_payload, keys: :atoms)
 
-    {:ok, event_to_send} = eval_submission(payload)
-      |> Jason.encode()
+    {:ok, event_to_send} = eval_submission(payload) |> Jason.encode()
     AMQP.Basic.publish(state.channel, "evaluated-submissions-topic", "", event_to_send)
 
     {:noreply, state}
   end
 
-  def eval_submission(%{code: _code, cases: cases, submission_id: submission_id}) do
-    # TODO - actual logic
-    results = cases
-    |> Enum.map(fn x -> "OK" end)
+  def eval_submission(%{code: code, cases: cases, submission_id: submission_id}) do
+    dir = "#{System.tmp_dir!()}\\submissions\\#{submission_id}"
+    File.mkdir_p!(dir)
+    code_file = "#{dir}\\code.cpp"
+    exe_file = "#{dir}\\code.exe"
+    File.write!(code_file, code)
+    System.cmd("g++", [code_file, "-o", exe_file]) # TODO - Compilation errors
 
-    SubmissionEvaluated.new(submission_id, results)
+    test_results = cases
+    |> Enum.with_index
+    |> Enum.map(fn ({test_case, id}) -> run_test(
+      exe_file,
+      test_case,
+      "#{dir}\\test-#{id}}"
+    ) end)
+
+    SubmissionEvaluated.new(submission_id, test_results)
+  end
+
+  defp run_test(
+    exe_file,
+    test_case,
+    input_file
+  ) do
+    File.write!(input_file, test_case.input)
+    # TODO - TL
+    res = :os.cmd(:"type #{input_file} | #{exe_file}") |> List.to_string()
+    actual = String.replace(res, ~r/(\r|\n)/, "")
+    expected = String.replace(test_case.output, ~r/(\r|\n)/, "")
+    if actual == expected do
+      "OK"
+    else
+      "WA"
+    end
   end
 
   def handle_info(msg, state) do
